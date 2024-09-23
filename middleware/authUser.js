9255f4a347f5135aaf227d8e0e29583ac498046a
sanarajan <sanamol87@gmail.com>
2024-09-23 22:32:41 +0530

    changes after aws hosting and error in google login

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -1,8 +1,9 @@
 // middleware/isUserAuthenticated.js
 function isUserAuthenticated(req, res, next) {
-  console.log("Checking authentication");
+  console.log("Checking authentication auth");
 
   if (req.session && req.session.useremail && req.session.userType === 2) {
+    console.log(" sssion pass")
     next();
   } else {
     // User is not authenticated, redirect to login page
